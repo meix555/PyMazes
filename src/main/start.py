af39b54c6f39dedba0b0d246535f2383ccd30b3c
meix555 <andreasmeixner@hotmail.com>
2016-01-03 11:30:06 +0100

    extracted dialog class to its own file

--- src/main/start.py
+++ src/main/start.py
@@ -1,39 +1,8 @@
 import sys
-from PyQt5 import QtWidgets, uic, QtGui, QtCore
 
-
-class PyMazesDialog(QtWidgets.QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = uic.loadUi("mazes.ui", self)
-        self.ui.drawMazeButton.clicked.connect(self.onDraw)
-
-        self.draw = True
-
-        self.pen = QtGui.QPen(QtGui.QColor(0, 0, 0))
-        self.pen.setWidth(3)
-        self.brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-
-
-    def paintEvent(self, event):
-        if self.draw:
-            painter = QtGui.QPainter(self)
-            painter.setPen(self.pen)
-            painter.setBrush(self.brush)
-            painter.drawRect(10, 70, 130, 130)
-
-            painter.drawLine(10, 150, 100, 150)
-
-
-    def onDraw(self):
-        index = self.ui.algorithmComboBox.currentIndex()
-
-        self.draw = not self.draw
-
-        self.update()
-
+from src.main.mazes_dialog import *
 
 app = QtWidgets.QApplication(sys.argv)
 dialog = PyMazesDialog()
-dialog.show();
+dialog.show()
 sys.exit(app.exec_())
