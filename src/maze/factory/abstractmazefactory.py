630f93e59e578ab2f7baf7d06f46ef5e56919827
meix <andreasmeixner@hotmail.com>
2016-06-18 23:25:21 +0200

    cell size moved from cell to renderer

--- src/maze/factory/abstractmazefactory.py
+++ src/maze/factory/abstractmazefactory.py
@@ -1,3 +1,3 @@
 class AbstractMazeFactory(object):
-    def create_maze(self, cell_size:int, maze_width:int, maze_height:int, mask_filename:str = None):
+    def create_maze(self, maze_width:int, maze_height:int, mask_filename:str = None):
         raise NotImplementedError("Should have implemented this")
